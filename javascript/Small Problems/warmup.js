f1368eb904e66ce450734d173e20cb8c5e0cf2df
Pooja0131 <to.poojamehta@gmail.com>
2016-05-02 15:01:11 -0400

    added vowel_count()

--- javascript/Small Problems/warmup.js
+++ javascript/Small Problems/warmup.js
@@ -1,11 +1,26 @@
-// Build a function which takes an array and returns the maximum number.
+//1. Build a function which takes an array and returns the maximum number.
 
 function getMaxOfArray(numArray) {
   // return Math.max.apply(null, numArray);  this also works
-	return Math.max(...numArray);
+  return Math.max(...numArray);
 }
 
-var array1 = [1,2,3,4,5,6,77,8,9,10];
+var array1 = [1, 2, 3, 4, 5, 6, 77, 8, 9, 10];
 
 var maximumNumber = getMaxOfArray(array1);
 console.log(maximumNumber);
+
+
+
+
+
+//2. Build a function vowel_count() which takes a string and returns the number of vowels
+var count = 0;
+
+function vowel_count(str) {
+  var matches = str.match(/[aeiou]/gi);
+	var count = matches ? matches.length : 0;
+	return count;
+}
+var str = "hello friend!";
+console.log("'" + str + "' has " + vowel_count(str) + " vowels");

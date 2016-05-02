ffacc60c75a0b502e43775f084c3c425db7fcabe
Pooja0131 <to.poojamehta@gmail.com>
2016-05-02 16:41:33 -0400

    added reverse()

--- javascript/Small Problems/warmup.js
+++ javascript/Small Problems/warmup.js
@@ -24,3 +24,20 @@ function vowel_count(str) {
 }
 var str = "hello friend!";
 console.log("'" + str + "' has " + vowel_count(str) + " vowels");
+
+
+
+//3. Build a function reverse() which takes a string and returns all the characters in the opposite position, e.g. reverse("this is a string") // "gnirts a si siht"
+
+function reverse(str){
+	var arr = str.split('');
+	var arr1 = [];
+	for(var i=arr.length, j=0; i>=0, j<=arr.length; i--, j++){
+			arr1[j] = arr[i];
+	}
+	return arr1;
+}
+
+var str = "hello friend";
+console.log("Original String : " + str);
+console.log("Reversed String : " + reverse(str).join(""));
